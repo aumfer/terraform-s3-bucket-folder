a3c944a60b8746969e233dc478449db45853fb58
Anthony-Umfer <Anthony.Umfer@coxautoinc.com>
2019-02-05 22:26:56 -0500

    generates `aws_s3_bucket_object` for all files in a directory tree

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # terraform-s3-bucket-folder
 generate `aws_s3_bucket_object` for all files in a directory
+
+`dotnet run --project terraform-s3-bucket-folder.csproj`
---
+++ terraform-s3-bucket-folder.cs
@@ -0,0 +1,58 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace terraform_s3_bucket_folder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bucketName = "${module.cdn.s3_bucket}";
+            var readPath = $"{Environment.GetEnvironmentVariable("CODEBUILD_SRC_DIR")}{Path.DirectorySeparatorChar}{Environment.GetEnvironmentVariable("TF_VAR_build_path")}";
+            var bucketPath = Environment.GetEnvironmentVariable("TF_VAR_branch_name");
+            Directory.CreateDirectory(readPath);
+
+            var writePath = $"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}{bucketName}.tf";
+
+            Console.WriteLine($"bucketName {bucketName}");
+            Console.WriteLine($"readPath {readPath}");
+            Console.WriteLine($"writePath {writePath}");
+
+            var files =
+                from searchFile in Directory.EnumerateFiles(readPath, "*", SearchOption.AllDirectories)
+                let filePath = Path.GetFullPath(searchFile)
+                select filePath;
+
+            var bucketObjects =
+                from absPath in files
+                let fileName = Path.GetFileName(absPath)
+                let hash = (uint)fileName.GetHashCode()
+                let relPath = Path.GetRelativePath(readPath, absPath)
+                let key = $"{bucketPath}{Path.DirectorySeparatorChar}{relPath}"
+                select new StringBuilder()
+                .AppendLine($"  resource {TerraformResource.AwsS3BucketObject} {TerraformResource.Quote($"file-{hash}")} {{")
+                .AppendLine($"  bucket = {TerraformResource.Quote(bucketName)}")
+                .AppendLine($"  key = {TerraformResource.Quote(key)}")
+                .AppendLine($"  source = {TerraformResource.Quote(absPath)}")
+                .AppendLine("}")
+                .AppendLine()
+                .ToString();
+
+            var lines = bucketObjects.ToArray();
+
+            File.WriteAllLines(writePath, lines);
+
+            Console.WriteLine(File.ReadAllText(writePath));
+        }
+
+        static class TerraformResource
+        {
+            public static string Quote(string s) => '"' + s + '"';
+            public static readonly string AwsS3BucketObject = Quote("aws_s3_bucket_object");
+        }
+    }
+}
